305ef4a72188b1deb7bd9a28d30f763019c5a95e
Khanh LeViet <khanhlvg@google.com>
2022-01-14 21:22:46 +0900

    Added sound classification example.

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 This repository contains examples of different ML use cases on Raspberry Pi.
 
+## Computer Vision
+
 * [Image Classification](image_classification/) 
 * [Object Detection](object_detection/) 
 * [Image Segmentation](image_segmentation/)
+
+## Audio
+
+* [Sound Classification](sound_classification/)
---
+++ sound_classification/audio_controller.py
@@ -0,0 +1,57 @@
+# Copyright 2021 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""A module get audio from device."""
+import threading
+import numpy as np
+import sounddevice as sd
+
+
+class AudioRecord(object):
+
+  def __init__(self, sampling_rate: int) -> None:
+    self._lock = threading.Lock()
+    self._audio_buffer = []
+
+    def audio_callback(indata, frames, time, status):
+      self._lock.acquire()
+      self._audio_buffer.append(indata[::1, [0]])
+      self._lock.release()
+
+    # Create an input stream to continuously capture the audio data.
+    self._stream = sd.InputStream(
+      channels=1,
+      samplerate=sampling_rate,
+      callback=audio_callback,
+    )
+
+  def start_recording(self) -> None:
+    """Start the input stream"""
+    self._stream.start()
+
+  def stop(self) -> None:
+    """Stop the input stream"""
+    self._audio_buffer = []
+    self._stream.stop()
+
+  @property
+  def buffer(self) -> np.ndarray:
+    self._lock.acquire()
+    if len(self._audio_buffer):
+      result = np.concatenate(self._audio_buffer)
+      self._audio_buffer.clear()
+    else:
+      result = np.zeros((0, 1))
+    self._lock.release()
+
+    return result
